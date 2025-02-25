45cd36bd3a3de9a9afdc61a534b73f9afb3e2966
FarkhodovIslom <111051883+FarkhodovIslom@users.noreply.github.com>
2025-02-25 19:54:54 +0500

    pachti tugatdim

--- controller/controller.js
+++ controller/controller.js
@@ -1,21 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
-function fetchData() {
-    const filePath = path.join(__dirname, "../data/parkedCars.json");
-    const data = fs.readFileSync(filePath, "utf8");
-    const cars = JSON.parse(data);
-    return cars;
-}
+const filePath = path.join(__dirname, "../data/parkedCars.json");
+const data = fs.readFileSync(filePath, "utf8");
+const cars = JSON.parse(data);
+
+console.log(cars);
 
 const pageController = {
     index: (req, res) => {
         res.render('index');
     },
     getCars: (req, res) => {
-        res.render('carList', fetchData());
+        res.render('carList', {cars});  
     }
 }
-
 
 module.exports = pageController;
